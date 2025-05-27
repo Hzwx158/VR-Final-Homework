4618ef9e5e6d173d52af77ff28186418f0ed4aa4
Hzwx158 <1583913466@qq.com>
2025-05-27 19:27:11 +0800

    Soyo Complete
    还差游戏结束逻辑

--- Assets/Scripts/SoyoController.cs
+++ Assets/Scripts/SoyoController.cs
@@ -1,47 +1,114 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SoyoController : MonoBehaviour
 {
     private Animator animator;
-    public float Theta;
+    public float SpinSpeed;
+    public float MoveSpeed;
+    private bool canMove;
+    private float spinSpeed;
+    public GameObject[] SoyoCtrl;
+    private Vector3 lastPos;
+    private bool recordPos = false;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        canMove = true;
+        spinSpeed = 0;
+        SoyoCtrl[0].GetComponent<XRGrabInteractable>().selectEntered.AddListener(MoveStraight);
+        SoyoCtrl[1].GetComponent<XRGrabInteractable>().selectEntered.AddListener(MoveLeft);
+        SoyoCtrl[2].GetComponent<XRGrabInteractable>().selectEntered.AddListener(MoveRight);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(animator.transform.position);
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.name.ToLower().Contains("wall"))
+        if(spinSpeed!=0)
         {
-            MoveStop();
+            transform.Rotate(0, spinSpeed*Time.deltaTime, 0);
         }
-        Debug.Log("stop!!!");
+        foreach (GameObject go in SoyoCtrl)
+        {
+            if (go.transform.position[1] <= 0){
+                var pos = go.transform.position;
+                go.transform.position = new Vector3(pos[0], 0.1f, pos[2]);
+            }
+        }
+        if(recordPos)
+        {
+            lastPos = transform.position;
+        }
+        recordPos = !recordPos;
     }
-    
-    public void MoveStraight()
+
+
+
+    private void OnTriggerEnter(Collider other)
     {
+        Debug.Log($"Trigger with Wall={other.gameObject.name.ToLower().Contains("wall")}!!!");
+        if (other.gameObject.name.ToLower().Contains("wall"))
+        {
+            canMove = false;
+            MoveStop();
+            var wallPos = other.gameObject.transform.position;
+            float deltaX = lastPos.x - wallPos.x;
+            float deltaZ = lastPos.z - wallPos.z;
+            transform.position = new Vector3(
+                wallPos[0] + Math.Sign(deltaX)*0.5f, 
+                transform.position.y, 
+                wallPos[2] + Math.Sign(deltaZ) * 0.5f
+            );
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Can Move");
+        canMove = true;
+    }
+
+    public void MoveStraight(SelectEnterEventArgs args)
+    {
+        var interactor = args.interactorObject as XRBaseInteractor;
+        Debug.Log(interactor.gameObject.name);
+        if (!interactor.gameObject.CompareTag("left"))
+            return;
+        if (!canMove)
+        {
+            return; 
+        }
+        Debug.Log("call MoveStraight");
+        animator.speed = MoveSpeed;
         animator.SetBool("ClickToMove", true);
     }
 
-    public void MoveLeft()
+    public void MoveLeft(SelectEnterEventArgs args)
     {
-        animator.transform.Rotate(new Vector3(0, -Theta, 0));
+        Debug.Log("call MoveLeft");
+        spinSpeed = -SpinSpeed;
     }
-    public void MoveRight()
+    public void MoveRight(SelectEnterEventArgs args)
     {
-        animator.transform.Rotate(new Vector3(0, Theta, 0));
+        Debug.Log("call MoveRight");
+        spinSpeed = SpinSpeed;
     }
     public void MoveStop()
     {
+        spinSpeed = 0;
+        Debug.Log("call MoveStop");
         animator.SetBool("ClickToMove", false);
-        animator.Play("mixamo_com", -1, float.NegativeInfinity);
+        animator.speed = 0;
+    }
+    public void SelectSummonRing()
+    {
+        var ringPos = SoyoCtrl[3].transform.position;
+        SoyoCtrl[0].transform.position = new Vector3(ringPos[0], ringPos[1]+0.5f, ringPos[2]);
+        SoyoCtrl[1].transform.position = new Vector3(ringPos[0], ringPos[1]+0.5f, ringPos[2] - 0.5f);
+        SoyoCtrl[2].transform.position = new Vector3(ringPos[0], ringPos[1]+0.5f, ringPos[2] + 0.5f);
     }
 }
